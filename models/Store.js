f0e507bc07ae76fa467f4e6c319f9135de69a397
Jordi Nieuwenburg <j.nieuwenburg@hva.nl>
2020-04-06 08:01:58 +0200

    Store.js added code that might fix update slug name

--- models/Store.js
+++ models/Store.js
@@ -35,20 +35,43 @@ const storeSchema = new mongoose.Schema({
   photo: String
 });
 
-storeSchema.pre('save', async function(next) {
-  if(!this.isModified('name')) {
-    next(); // skip it
-    return; // stop this function from running
-  }
-  this.slug = slug(this.name);
-  // find other stores that have a slug of wes, wes-1, wes-2
-  const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
-  const storesWithSlug = await this.constructor.find({ slug: slugRegEx });
-  if(storesWithSlug.length) {
-    this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+// storeSchema.pre('save', async function(next) {
+//   if(!this.isModified('name')) {
+//     next(); // skip it
+//     return; // stop this function from running
+//   }
+//   this.slug = slug(this.name);
+//   // find other stores that have a slug of wes, wes-1, wes-2
+//   const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+//   const storesWithSlug = await this.constructor.find({ slug: slugRegEx });
+//   if(storesWithSlug.length) {
+//     this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+//   }
+
+//   next();
+// });
+
+storeSchema.pre('findOneAndUpdate', async function(next) {
+
+  // Find and compare original details to new ones.
+  const store = await this.findOne({});
+  if(!this._update.name || store.name === this._update.name) {
+    return next();
   }
 
+  // See if any stores with slug already exist.
+  let   storeSlug = slug(this._update.name);
+  const slugRegEx = new RegExp(`^(${storeSlug})((-[0-9]*$)?)$`, 'i');
+  const storesWithSlug = await Store.find({slug: slugRegEx});
+
+  // Set slug with incremement if store exists.
+  if(storesWithSlug.length) storeSlug = `${storeSlug}-${storesWithSlug.length + 1}`;
+
+  // Update store with new slug.
+  const newSlug = promisify(this.update({}, { slug: storeSlug }), this);
+  await newSlug;
   next();
+
 });
 
 module.exports = mongoose.model('Store', storeSchema);
