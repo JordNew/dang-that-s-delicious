d381372935dcb684133326da4e2404eed880d301
Jordi Nieuwenburg <j.nieuwenburg@hva.nl>
2020-04-10 12:55:54 +0200

    registration handling and verification

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,4 +6,26 @@ exports.loginForm = (req, res) => {
 
 exports.registerForm = (req, res) => {
   res.render('register', { title: 'Register' });
-}
+}; 
+
+exports.validateRegister = (req, res, next) => {
+  req.sanitizeBody('name');
+  req.checkBody('name', 'Please supply a name!').notEmpty();
+  req.checkBody('email', 'Please provide a valid email address!').isEmail();
+  req.sanitizeBody('email').normalizeEmail({
+    remove_dots: false,
+    remove_extension: false,
+    gmail_remove_subaddress: false
+  });
+  req.checkBody('password', 'Password cannot be blank!').notEmpty();
+  req.checkBody('password-confirm', 'Confirmed password cannot be blank!').notEmpty();
+  req.checkBody('password-confirm', 'Oops! Your passwords do not match').equals(req.body.password);
+
+  const errors = req.validationErrors();
+  if (errors) {
+    req.flash('error', errors.map(err => err.msg));
+    res.render('register', { title: 'Register', body: req.body, flashes: req.flash() });
+    return; // stop the function from running
+  }
+  next(); // there were no errors!
+};
--- routes/index.js
+++ routes/index.js
@@ -36,4 +36,9 @@ router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 router.get('/login', userController.loginForm);
 router.get('/register', userController.registerForm);
 
+// 1. Validate the registration data
+// 2. register the user
+// 3. log the user in
+router.post('/register', userController.validateRegister);
+
 module.exports = router;
