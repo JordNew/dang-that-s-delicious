bda312c52af9de08c6c468b7f4048fd7a76baa53
ab_jnieuwe1 <j.nieuwenburg@hva.nl>
2020-05-11 08:03:04 +0200

    login doublecheck (to handle incognito access to hearts page)

--- routes/index.js
+++ routes/index.js
@@ -58,7 +58,7 @@ router.post('/account/reset/:token',
   catchErrors(authController.update)) // go ahead and actually update the password
 
 router.get('/map', storeController.mapPage);
-router.get('/hearts', catchErrors(storeController.getHearts));
+router.get('/hearts', authController.isLoggedIn, catchErrors(storeController.getHearts));
 
 /*
   API
